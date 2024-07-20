652e5b44d1e04f4f5c499beca09a8774284ccfc2
ron12373 <147504175+ron12373@users.noreply.github.com>
2024-07-20 13:02:01 +0700

    Update Userscript.js

--- baconbutpro/Userscript.js
+++ baconbutpro/Userscript.js
@@ -2,7 +2,7 @@
 // @name         Bypass Key System
 // @homepageURL  https://discord.gg/gFNAH7WNZj
 // @namespace    https://greasyfork.org/en/scripts/493960-arceus-bypass
-// @version      1.8.1
+// @version      1.8.2
 // @description  Bypass Arceus, Hydrogen, Delta, Trigon, Vegax, Codex, Linkvertise
 // @author       Bacon But Pro
 // @match        https://linkvertise.com/376138/arceus-x-neo-key-system-1?o=sharing*
@@ -30,7 +30,8 @@
 // @match        https://*.hcaptcha.com/*captcha*
 // @require     https://greasyfork.org/scripts/464929-module-jquery-xiaoying/code/module_jquery_XiaoYing.js
 // @require     https://greasyfork.org/scripts/464780-global-module/code/global_module.js
-// @require     https://raw.githubusercontent.com/Bacon-But-Pro/Userscript.js/main/Bacon/Userscript.js
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js
+// @require     https://raw.githubusercontent.com/Bacon-But-Pro/Userscript.js/main/Main.js
 
 // @run-at       document-end
 // @grant        GM_xmlhttpRequest
